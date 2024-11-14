5e57b6368e21b982863ab0c4416ba49c6723e8d1
POTATOPROGAMMER <codercode1010@gmail.com>
2024-11-14 17:43:45 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,19 @@
+x=15
+if (type (x) is int ) :
+    print ("true")
+else:
+     print ("false")
+
+x=15
+if (type (x) is str ) :
+    print ("true")
+else:
+     print ("false")
+
+
+
+x=15
+if (type (x) is not int ) :
+    print ("true")
+else:
+     print ("false")
